f7dd759c44fbe7fa73dfc44d320c8748c1a4d9b8
Cædrik Labrecque <caedrikbl@gmail.com>
2025-02-19 14:35:41 -0500

    layout

--- app/layout.js
+++ app/layout.js
@@ -29,9 +29,27 @@ export default function RootLayout({ children }) {
     return (
         <html lang="fr">
             <body
-                className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#784fed] ${nunito.variable} overflow-hidden`}
+                className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#784fed] ${nunito.variable} `}
             >
-                <div className="isolate text-white/80">
+                <Image
+                    alt="background image"
+                    src={
+                        "https://images.unsplash.com/photo-1603306557434-284b08b2e0c2?q=80&w=2076&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2830&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"
+                    }
+                    fill
+                    className="fixed top-0 w-screen h-screen object-cover opacity-60"
+                />
+                <div className="opacity-40 mix-blend-screen fixed top-0 h-screen w-screen overflow-hidden pointer-events-none">
+                    <video
+                        className="absolute top-1/2 left-1/2 w-auto h-auto -translate-x-1/2 -translate-y-1/2 min-h-screen min-w-screen object-cover z-0"
+                        autoPlay
+                        loop
+                        muted
+                    >
+                        <source src="file.mp4" type="video/mp4" />
+                    </video>
+                </div>
+                <div className="isolate text-white/80 min-h-screen flex-col">
                     <header className="relative top-0 z-50 w-full">
                         <nav className="flex flex-1 items-center justify-between p-6 xl:py-8">
                             <div className="flex-col items-center px-8">
@@ -47,28 +65,10 @@ export default function RootLayout({ children }) {
                             </div>
                         </nav>
                     </header>
-                    <Image
-                        alt="background image"
-                        src={
-                            "https://images.unsplash.com/photo-1603306557434-284b08b2e0c2?q=80&w=2076&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2830&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"
-                        }
-                        fill
-                        className="fixed inset-0 w-full h-full object-cover -z-10 opacity-80"
-                    />
-                    <div className="mix-blend-luminosity opacity-20 absolute top-0 h-screen w-screen overflow-hidden pointer-events-none">
-                        <video
-                            className="absolute top-1/2 left-1/2 w-auto h-auto -translate-x-1/2 -translate-y-1/2 min-h-screen min-w-screen object-cover z-0"
-                            autoPlay
-                            loop
-                            muted
-                        >
-                            <source src="file.mp4" type="video/mp4" />
-                        </video>
-                    </div>
 
                     <div
                         className="
-                    max-w-2xl pb-56 mx-12"
+                    max-w-2xl md:pb-40 mx-12"
                     >
                         {children}
                     </div>
